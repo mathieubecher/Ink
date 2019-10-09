ea76f443a6e5ea75b4c6e430d7211e2e30974e82
Becher <mbelkhenchir@gmail.com>
2019-10-09 15:01:04 +0200

    Find dead & create anim

--- Assets/Script/Dead.cs
+++ Assets/Script/Dead.cs
@@ -6,7 +6,7 @@ public class Dead : MonoBehaviour
 {
     private static Dead currentdead;
     private Collider2D cursor;
-    private float deadTime;
+    public float deadTime;
     public string text;
     public bool active = false;
     // Start is called before the first frame update
@@ -18,7 +18,7 @@ public class Dead : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        deadTime += Time.deltaTime;
+        deadTime += Time.deltaTime/(float)(60);
         if(cursor != null)
         {
             if (cursor.Distance(GetComponent<Collider2D>()).distance > 0)
--- Assets/Script/InitializeGame.cs
+++ Assets/Script/InitializeGame.cs
@@ -20,6 +20,8 @@ public class InitializeGame : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject dead;
+    public float miny =-1.9f;
+    public float maxy =-5f;
     void Start()
     {
         StartCoroutine(Register());
@@ -41,8 +43,15 @@ public class InitializeGame : MonoBehaviour
 
         foreach(DeadInfo deadinfo in json.Deads)
         {
-            GameObject deadObject = Instantiate(dead,new Vector3(deadinfo.position,0,0), Quaternion.identity);
+            GameObject deadObject = Instantiate(dead,new Vector3(deadinfo.position, (Random.value * (maxy -miny)) + miny, 0), Quaternion.identity);
             (deadObject.GetComponent(typeof(Dead)) as Dead).text = deadinfo.text;
+            Debug.Log(deadinfo.dead );
+            System.DateTime deaddate = System.DateTime.ParseExact(deadinfo.dead, "yyyy-dd-MM hh:mm:ss",null);
+
+            //(deadObject.GetComponent(typeof(Dead)) as Dead).deadTime = (System.DateTime.Now - deaddate).Minutes;
+            //Debug.Log(System.DateTime.Now);
+            //Debug.Log(deadinfo.dead + " " + deaddate);
+            Debug.Log((deadObject.GetComponent(typeof(Dead)) as Dead).deadTime);
         }
         
 
